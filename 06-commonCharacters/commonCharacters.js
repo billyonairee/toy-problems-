c09083f5e389aa8f34000447cc8cdc8729a94542
이용석 <leeyongseok@iyongseog-ui-MacBook-Pro.local>
2018-10-05 10:03:58 +0900

    toy06

--- 06-commonCharacters/commonCharacters.js
+++ 06-commonCharacters/commonCharacters.js
@@ -11,5 +11,26 @@
  */
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  var res = [];
+  // var strArr = []
+  // for(var key in arguments){
+  //   strArr.push(arguments[key])
+  // }
+  // strArr.sort((a,b)=>{return b.length - a.length})
+
+  if(string1 === ""){return ""}
+  
+  if(string1.length < string2.length){
+    longstr = string2.split('')
+    shortstr = string1.split('')
+  }else {
+    longstr = string1.split('');
+    shortstr = string2.split('');
+  }
+  for(var i = 0; i < longstr.length; i++){
+      if(shortstr.includes(longstr[i]) && !res.includes(longstr[i])&& longstr[i] !== " "){
+        res.push(longstr[i])
+    }
+  }
+  return res.join('')
 };
