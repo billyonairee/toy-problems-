12a879685fa90704070125a5825b0f831534b05b
이용석 <leeyongseok@iyongseog-ui-MacBook-Pro.local>
2018-10-10 09:48:22 +0900

    toy08

--- 08-largestProductOfThree/largestProductOfThree.js
+++ 08-largestProductOfThree/largestProductOfThree.js
@@ -7,7 +7,18 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-
 var largestProductOfThree = function(array) {
-  // TODO: everything
-};
+  if (array.every(el => el < 0)){
+    var sortedArr = array.sort(function(a, b) {return b - a;});
+    return sortedArr[0] * sortedArr[1] * sortedArr[2];
+  }
+  var sortedArr = array.sort(function (a, b) { return Math.abs(b) - Math.abs(a); });
+  for(var i = 2; i < sortedArr.length; i++){
+    res = sortedArr[0] * sortedArr[1] * sortedArr[i];
+    if(res >= 0){
+      return res
+    }
+  }
+}
+
+console.log(largestProductOfThree([-31, 41, 34, -37, -17, 29]));
