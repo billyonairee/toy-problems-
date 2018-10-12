3815c69bd5ce01734feca8e1767b1b59879e50fe
이용석 <leeyongseok@iyongseog-ui-MacBook-Pro.local>
2018-10-12 10:13:32 +0900

    toy10

--- 10-htmljQueryColor/main.js
+++ 10-htmljQueryColor/main.js
@@ -6,11 +6,25 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  var p = $("p").text().trim().split(" ");
+
+  for(var i = 0; i < p.length; i++) {
+      p[i] = `<span>${p[i]}</span> `
+  }
+  $('p').html(p)
 
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
+  var colorChange = function () {
+    var rgbColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+    $('span').css('color',rgbColor)
+  }
 
+   setInterval(function()
+    {
+      colorChange()
+    },1000);
 });
