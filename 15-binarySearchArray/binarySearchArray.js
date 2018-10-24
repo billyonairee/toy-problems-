420c2d098e84b4cb57fa68e5958eb034dce95be5
Yongseok <leeyongseok@iyongseog-ui-MacBook-Pro.local>
2018-10-24 14:05:04 +0900

    toy15

--- 15-binarySearchArray/binarySearchArray.js
+++ 15-binarySearchArray/binarySearchArray.js
@@ -8,6 +8,22 @@
  * console.log(index); // [3]
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function (array, target, start, end) {
+  var start = start || 0;
+  var end = end || array.length-1;
+  var mid = Math.floor((start + end)/2);
+  if(array[mid] === target){
+    return mid
+  }else if(array[mid] < target){
+    return binarySearch(array, target, mid +1, end)
+  }else if(array[mid] > target){
+    return binarySearch(array, target, start, mid -1)
+  }
+  return null
 };
+
+
+
+
+
 
